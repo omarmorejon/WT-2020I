834e3cca8286bf02ba08dcae13da59a7fe2d969c
alopezag <lopez.angel@gmail.com>
2020-05-17 13:11:37 +0200

    DOM & FW

---
+++ unit02/activity5/frameworks/node_parseJSON.js
@@ -0,0 +1,9 @@
+//Following example helps you to use JSON.parse() function and access the elements from JSON Object
+// json data
+var jsonData = '{"persons":[{"name":"John","city":"New York"},{"name":"Phil","city":"Ohio"}]}';
+ 
+// parse json
+var jsonParsed = JSON.parse(jsonData);
+ 
+// access elements
+console.log(jsonParsed.persons[0].name);
